77a654a08c2d197494cb875743a45808383b94af
Spece <Michael@Spece.AI>
2023-10-22 13:45:09 -0400

    #2 Unify versions.

--- src/captcha_solver/__init__.py
+++ src/captcha_solver/__init__.py
@@ -1,5 +1,6 @@
 # pylint: disable=wildcard-import
 from captcha_solver.solver import CaptchaSolver
 from captcha_solver.error import *  # noqa
+from pkg_resources import get_distribution
 
-__version__ = '0.1.5'
+__version__ = get_distribution("Humanoid").version
