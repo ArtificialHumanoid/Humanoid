d31c5ba5aa243e0fb6883f3948448b2f1a9b8d6a
Spece <Michael@Spece.AI>
2024-10-01 17:20:12 -0400

    #3 Monkey patch.

--- build/setup.py
+++ build/setup.py
@@ -1,13 +1,29 @@
-from setuptools import setup
+import os
 
-with open("../README.md", "r", encoding='UTF-8') as fh:
+from setuptools import setup
+from setuptools.config import expand
+
+if __debug__:
+    print(os.getcwd())
+
+with open("../README.md", "r", encoding="UTF-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding='UTF-8') as fh:
+with open("requirements.txt", "r", encoding="UTF-8'") as fh:
     requirements = fh.read().split("\n")
 
+# Monkey patch.
+
+
+def _assert_local(_, __):
+    return True
+
+
+expand._assert_local = _assert_local
+
+
 setup(
-    version="0.0.0",
+    version="0.0.1",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[requirements],
