7de2572dd04e0206356b984f8267457810808a72
MichaelSpece <Michael@Spece.AI>
2025-06-24 13:39:51 +0000

    #2 Correct importation path.

--- src/humanoid/captcha_solver/__init__.py
+++ src/humanoid/captcha_solver/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=wildcard-import
 from humanoid.captcha_solver.solver import CaptchaSolver
-from captcha_solver.error import *  # noqa
+from humanoid.captcha_solver.error import *  # noqa
 from pkg_resources import get_distribution
 
 __version__ = get_distribution("Humanoid").version
